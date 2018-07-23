732ca42a5b8051cccb065b052efffa60f1134862
John Roche <jroche@redhat.com>
2018-07-23 13:55:46 +0100

    Add client tabs

--- ui/src/Client.js
+++ ui/src/Client.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { Nav, NavItem, TabContent, TabPane, Tabs } from 'patternfly-react';
 
 class Client extends Component {
   render() {
     return (
       <div>
-        <div>Client view</div>
+        <div>
+            <Tabs id="basic-tabs-pf" defaultActiveKey={1}>
+                <div>
+                <Nav bsClass="nav nav-tabs nav-tabs-pf nav-justified">
+                    <NavItem eventKey={1}>Configuration</NavItem>
+                    <NavItem eventKey={2}>Builds</NavItem>
+                    <NavItem eventKey={3}>Mobile Services</NavItem>
+                </Nav>
+                    <TabContent>
+                        <TabPane eventKey={1}>
+                            Configuration
+                        </TabPane>
+                        <TabPane eventKey={2}>
+                            Builds
+                        </TabPane>
+                        <TabPane eventKey={3}>
+                            Mobile Services
+                        </TabPane>
+                    </TabContent>
+                </div>
+            </Tabs>
+        </div>
       </div>
     );
   }
