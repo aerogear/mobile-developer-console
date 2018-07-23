bf671353c505b5b48e312c2f0a494acca501af0b
John Roche <jroche@redhat.com>
2018-07-23 13:46:24 +0100

    Add Routing

---
+++ ui/src/Client.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Client extends Component {
+  render() {
+    return (
+      <div>
+        <div>Client view</div>
+      </div>
+    );
+  }
+}
+
+export default Client;
