f5830b89764fe23ea66df20b02a7d90b75b20c1a
John Roche <jroche@redhat.com>
2018-07-23 13:46:24 +0100

    Add Routing

---
+++ ui/src/Overview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+  render() {
+    return (
+      <div>
+        <div>Overview</div>
+      </div>
+    );
+  }
+}
+
+export default Overview;
