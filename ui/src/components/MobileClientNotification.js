f6b227d5dbbc0e1b43bfbbc5a2e8ec88b823942d
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-07-26 16:49:07 +0100

    add mobile client notification component

---
+++ ui/src/components/MobileClientNotification.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Alert } from 'patternfly-react'
+
+let view = true;
+class MobileClientNotification extends Component {
+    constructor(props) {
+        super(props)
+        this.view = true;
+        this.handleDismiss = this.handleDismiss.bind(this)
+    }
+
+    // handler for alert dismiss button
+    handleDismiss() {
+        this.setState({})
+        view = false;
+    }
+    
+    // handle to see if an alert is needed
+    handleAlert() {
+        return (this.props.unBoundServices === 0 || !view) ? "" : 
+        <Alert type="info" onDismiss={this.handleDismiss}>{this.props.unBoundServices} mobile services are not bound to this client. <a>Bind them to use with this client.</a></Alert>;
+    }
+
+    render = () => {
+        return (
+            this.handleAlert()
+        )
+    }
+}
+
+export default MobileClientNotification;
