cb242cf739b8c1acb07097240472591306237b04
Stephen Coady <scoady@redhat.com>
2018-07-30 18:21:41 +0100

    style changes

---
+++ ui/src/components/build/BuildConfig.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'patternfly-react';
+
+import './BuildConfig.css';
+
+class BuildConfig extends Component {
+  render() {
+    const {repoUrl, branch, jobName, jenkinsfilePath} = this.props.buildConfig;
+
+    return (
+      <div className="build-config">
+        <Row>
+          <Col md={6}>
+            <b>Repo Url</b>
+            <a href={repoUrl}>{repoUrl}</a>
+          </Col>
+          <Col md={6}>
+            <b>Branch</b>
+            <span>{branch}</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <b>Jenkins Job Name</b>
+            <span>{jobName}</span>
+          </Col>
+          <Col md={6}>
+            <b>Jenkinsfile Path</b>
+            <span>{jenkinsfilePath}</span>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default BuildConfig;
