2daef7d70ec2e0c71ab5f1e6f30bb8d31c4fe813
Jan Hellar <jhellar@redhat.com>
2018-07-30 11:00:40 +0200

    Use scss instead of inline styling

--- ui/src/components/ComponentLabel.js
+++ ui/src/components/ComponentLabel.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 
+import './ComponentLabel.css';
+
 class ComponentLabel extends Component {
   render() {
     return (
-      <div style={{
-        paddingTop: 15,
-        borderBottomWidth: 1,
-        borderBottomStyle: 'solid',
-        borderBottomColor: '#dcdcdc',
-        lineHeight: 1,
-        marginBottom: 10,
-        paddingBottom: 5,
-        alignSelf: 'center',
-        color: '#757575',
-        fontSize: 10,
-        paddingRight: 10,
-        textTransform: 'uppercase'
-      }}>
+      <div className="component-label">
         {this.props.children}
       </div>
     );
