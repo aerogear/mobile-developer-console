60340f9b565dc62eeddd8b5d5ecd6d4881b45c60
Jan Hellar <jhellar@redhat.com>
2018-07-30 11:00:40 +0200

    Use scss instead of inline styling

--- ui/src/components/BuildConfig.js
+++ ui/src/components/BuildConfig.js
@@ -2,36 +2,32 @@ import React, { Component } from 'react';
 import { Row, Col } from 'patternfly-react';
 import ComponentLabel from './ComponentLabel';
 
-const titleStyle = {
-  display: 'inline-block',
-  marginRight: 40,
-  width: 100
-};
+import './BuildConfig.css';
 
 class BuildConfig extends Component {
   render() {
     return (
-      <div>
+      <div className="build-config">
         <ComponentLabel>
           Build Config
         </ComponentLabel>
         <Row>
-          <Col sm={12} md={12} lg={6}>
-            <b style={titleStyle}>Repo Url:</b>
+          <Col lg={6}>
+            <b>Repo Url:</b>
             <span>{this.props.repoUrl}</span>
           </Col>
-          <Col sm={12} md={12} lg={6}>
-            <b style={titleStyle}>Branch:</b>
+          <Col lg={6}>
+            <b>Branch:</b>
             <span>{this.props.branch}</span>
           </Col>
         </Row>
         <Row>
-          <Col sm={12} md={12} lg={6}>
-            <b style={titleStyle}>Build Platform:</b>
+          <Col lg={6}>
+            <b>Build Platform:</b>
             <span>{this.props.platform}</span>
           </Col>
-          <Col sm={12} md={12} lg={6}>
-            <b style={titleStyle}>Jenkinsfile Path:</b>
+          <Col lg={6}>
+            <b>Jenkinsfile Path:</b>
             <span>{this.props.jenkinsfilePath}</span>
           </Col>
         </Row>
