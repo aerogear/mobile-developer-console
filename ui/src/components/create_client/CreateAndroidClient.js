54d2420ea771c22998df7001aac61dc1dcc242ed
Vojtěch Sázel <vojtech@sazel.cz>
2018-08-01 13:23:07 +0200

    Mobile client creation dialog in wizard

--- ui/src/components/create_client/CreateAndroidClient.js
+++ ui/src/components/create_client/CreateAndroidClient.js
@@ -1,52 +1,51 @@
 import React, { Component } from 'react';
-import { Form  } from 'patternfly-react';
-import { renderForm } from './CreateClientFormUtils';
+import { Form } from 'patternfly-react';
+import { renderForm, validateId, validateAppName } from './CreateClientFormUtils';
 
 /**
  * Component for the Android specific create mobile client form.
  */
 class CreateAndroidClient extends Component {
+
+    state={}
+
+    formChanged = (id, change) => {
+        let state = {}
+        state[id] = change
+        this.setState(state)
+    }
+
     render() {
         const formFields = [
             {
-                controlId:'appName',
-                label:'* Application Name',
+                controlId: 'appName',
+                label: '* Application Name',
                 useFieldLevelHelp: true,
-                content:'Enter application name (like <em>myapp</em>)',
+                content: 'Enter application name (like <em>myapp</em>)',
                 formControl: ({ validationState, ...props }) => (
                     <Form.FormControl type="text" {...props} />
-                  )
+                ),
+                formGroupProps: {
+                    validationState: validateId(this.state ? this.state.appName : "")
+                },
+                onChange: (e) => this.formChanged('appName', e.target.value)
             },
             {
-                controlId:'appIdentifier',
-                label:'* Package Name',
+                controlId: 'appIdentifier',
+                label: '* Package Name',
                 useFieldLevelHelp: true,
-                content:'Enter package name (like <em>org.aerogear.android.myapp</em>)',
+                content: 'Enter package name (like <em>org.aerogear.android.myapp</em>)',
                 formControl: ({ validationState, ...props }) => (
                     <Form.FormControl type="text" {...props} />
-                  )
+                ),
+                formGroupProps: {
+                    validationState: validateAppName(this.state ? this.state.appIdentifier : "")
+                },
+                onChange: (e) => this.formChanged('appIdentifier', e.target.value)
             },
-        ] 
-        const formButtons = [
-            {
-              text: 'Create app',
-              bsStyle: 'primary',
-              onClick: (e)=> { 
-                  //TODO
-                  alert('create')
-              }
-            },
-            {
-              text: 'Cancel',
-              bsStyle: 'default',
-              onClick: (e)=>{
-                  //TODO
-                  alert('cancel')
-              }
-            }
-          ];
-                 
-        return renderForm(formFields,formButtons);
+        ]
+
+        return renderForm("Configure Android App", formFields);
     }
 
 }
--- ui/src/components/create_client/CreateClient.js
+++ ui/src/components/create_client/CreateClient.js
@@ -1,57 +1,144 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, TabContent, TabPane, Tabs } from 'patternfly-react';
+import { Grid, Wizard, Button } from 'patternfly-react';
 import PlatformItem from './PlatformItem'
 import CreateAndroidClient from './CreateAndroidClient'
 import CreateCordovaClient from './CreateCordovaClient'
 import CreateIOSClient from './CreateIOSClient'
+import CreateXamarinClient from './CreateXamarinClient'
+import { ResultsLine } from './ResultsLine';
 
 /**
  *  Component for the mobile client creation.
  */
 class CreateClient extends Component {
+
+    constructor(props) {
+        super(props)
+        this.baseState=this.state;
+    }
+
+
+    steps = ()=> [
+        {
+            title: 'Select mobile client platform',
+            render: () => <div>
+                <h2>Select mobile client platform</h2>
+                <Grid bsClass="platform-items">
+                    <Grid.Row>
+                        <Grid.Col sm={6} md={2} >
+                            <PlatformItem title="Android App" id="platform-android" inclass="fa fa-android" itemSelected={this.selectPlatform}/>
+                        </Grid.Col>
+                        <Grid.Col sm={6} md={2}>
+                            <PlatformItem title="Cordova App" id="platform-cordova" itemSelected={this.selectPlatform}>
+                                <span><img src="../../img/cordova.png" alt="Cordova" /></span>
+                            </PlatformItem>
+                        </Grid.Col>
+                        <Grid.Col sm={6} md={2} >
+                            <PlatformItem title="iOS App" id="platform-ios" inclass="fa fa-apple" itemSelected={this.selectPlatform} />
+                        </Grid.Col>
+                        <Grid.Col sm={6} md={2}>
+                            <PlatformItem title="Xamarin App" id="platform-xamarin" itemSelected={this.selectPlatform}>
+                                <span><img src="../../img/xamarin.svg" alt="Xamarin" /></span>
+                            </PlatformItem>
+                        </Grid.Col>
+                    </Grid.Row>
+                </Grid>
+            </div>,
+        }, {
+            title: 'Mobile client parameters',
+            render: this.renderClientSpecificForm                
+        }, {
+            title: 'Result',
+            render: () => <div>
+                <ResultsLine iconClass="fa fa-clock-o" text="App is being provisioned."/>                
+            </div>
+        }
+    
+    ]
+    
+
+    state = {
+        showModal: false,
+        loading: false,
+        activeStepIndex: 0
+    }
+
+    selectPlatform = (state)=> {
+        this.setState({activeStepIndex:1,platform:state.id})        
+    }
+
+    close = () => {
+        this.setState({ showModal: false, loading: false });
+    }
+
+    open = () => {
+        this.setState(this.baseState)
+        this.setState({ showModal: true, loading: false })
+    };
+
+    configureClient = (state) => {
+        this.setState({clientConfiguration:state.clientConfiguration})
+    }
+
+    renderClientSpecificForm= () => { 
+        switch (this.state.platform) {
+            case 'platform-android':
+                return <CreateAndroidClient configureClient={this.configureClient}/>     
+            case 'platform-cordova':
+                return <CreateCordovaClient configureClient={this.configureClient}/>
+            case 'platform-ios':
+                return <CreateIOSClient configureClient={this.configureClient}/>
+            case 'platform-xamarin':
+                return <CreateXamarinClient configureClient={this.configureClient}/>
+            default:
+                break;
+        }
+    }
+
+    
+    nextStep = () => {
+        if (this.state.activeStepIndex<this.steps().length -1 )
+        this.setState({activeStepIndex:this.state.activeStepIndex+1})        
+    }
+    
+    backStep = () => {
+        if (this.state.activeStepIndex>0 )  {
+            this.setState({activeStepIndex:this.state.activeStepIndex-1})        
+        }
+    }
+    
+    stepChanged = (step) => {
+
+    }
+
     render() {
         return (
             <div>
-                <Tabs id="basic-tabs-pf" defaultActiveKey={1}>
-                    <div>
-                    <Nav bsClass="nav nav-tabs nav-tabs-pf nav-justified">
-                        <NavItem eventKey={1}>
-                            <PlatformItem title="Android App" class="fa fa-android"/>
-                        </NavItem>
-                        <NavItem eventKey={2}>
-                            <PlatformItem title="Cordova App">
-                            <span><img src="../../img/cordova.png" alt="Cordova"/></span>
-                            </PlatformItem>
-                        </NavItem>
-                        <NavItem eventKey={3}>
-                            <PlatformItem title="iOS App" class="fa fa-apple"/>
-                        </NavItem>
-                        <NavItem eventKey={4}>
-                            <PlatformItem title="Xamarin App">
-                            <span><img src="../../img/xamarin.svg" alt="Xamarin"/></span>   
-                            </PlatformItem>
-                        </NavItem>
-                </Nav>
-                    <div class="create-client-tabcontent">
-                    <TabContent>
-                        <TabPane eventKey={1}>
-                            <CreateAndroidClient/>
-                        </TabPane>
-                        <TabPane eventKey={2}>
-                            <CreateCordovaClient/>
-                        </TabPane>
-                        <TabPane eventKey={3}>
-                            <CreateIOSClient/>
-                        </TabPane>
-                        <TabPane eventKey={4}>
-                            <CreateXamarinClient/>
-                        </TabPane>
-                    </TabContent>
-                    </div>
-                </div>
-            </Tabs>
-        </div>
-      )
+                <Button bsStyle="primary" bsSize="large" onClick={this.open}>
+                    Launch Stateless Wizard
+                </Button>
+                <Wizard.Pattern
+                    show={this.state.showModal}
+                    onHide={this.close}
+                    onExited={this.close}
+                    title="Create mobile client"
+                    shouldDisableNextStep={activeStepIndex => false}
+                    steps={this.steps()}
+                    loadingTitle="Creating mobile client..."
+                    loadingMessage="This may take a minute."
+                    loading={this.state.loading}
+                    stepButtonsDisabled={true}
+                    nextStepDisabled={this.state.activeStepIndex==0 && this.state.platform===undefined}
+                    prevStepDisabled={this.state.activeStepIndex>1}
+                    onStepChanged={this.stepChanged}
+                    nextText={this.state.activeStepIndex==1?"Create":"Next"}
+                    onNext={this.nextStep}
+                    onBack={this.backStep}
+                    activeStepIndex={this.state.activeStepIndex}                    
+                />
+
+            </div>
+        )
     }
 }
 
