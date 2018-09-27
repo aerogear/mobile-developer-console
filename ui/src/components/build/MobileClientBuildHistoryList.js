7008fdad4d85fd9e37b1de18d607d2baf9678c25
Pavel Šturc <psturc@redhat.com>
2018-09-27 13:43:53 +0200

    feat: links to jenkins build logs

--- ui/src/components/build/MobileClientBuildHistoryList.js
+++ ui/src/components/build/MobileClientBuildHistoryList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Col, Button } from 'patternfly-react';
 import Moment from 'react-moment';
 import moment from 'moment';
@@ -7,61 +7,72 @@ import BuildStatus from '../common/BuildStatus';
 
 import './MobileClientBuildHistoryList.css';
 
-class MobileClientBuildHistoryList extends Component {
-    render = () => {
-      const mobileClientBuilds = Object.values(this.props.mobileClientBuilds || {});
-      return (
-        <React.Fragment>
-          {mobileClientBuilds.map((mobileClientBuild) => {
-            const phase = mobileClientBuild.status.phase;
-            const buildNumber = mobileClientBuild.metadata.annotations['openshift.io/build.number'];
-            const durationString = moment.preciseDiff(mobileClientBuild.status.startTimestamp, mobileClientBuild.status.completionTimestamp);
+const MobileClientBuildHistoryList = (props) => {
+  const mobileClientBuilds = Object.values(props.mobileClientBuilds || {});
+  return (
+    <React.Fragment>
+      {mobileClientBuilds.map((mobileClientBuild) => {
+        const {
+          status: { phase, startTimestamp, completionTimestamp },
+          metadata: {
+            annotations: {
+              'openshift.io/build.number': buildNumber,
+              'openshift.io/jenkins-console-log-url': jenkinsConsoleLogUrl,
+            },
+            uid,
+          },
+        } = mobileClientBuild;
+        const durationString = moment.preciseDiff(startTimestamp, completionTimestamp);
 
-            return (
-              <Col xs={12} className="mobile-client-build-history-item" key={mobileClientBuild.metadata.uid}>
-                <Col xs={9} sm={10} md={10} className="status-panel">
-                  <div className="build-summary">
-                    <div>
-                      <BuildStatus build={mobileClientBuild} />&nbsp;
-                      <a>
-Build #
-                        {buildNumber}
+        return (
+          <Col xs={12} className="mobile-client-build-history-item" key={uid}>
+            <Col xs={9} sm={10} md={10} className="status-panel">
+              <div className="build-summary">
+                <div>
+                  <BuildStatus build={mobileClientBuild} />&nbsp;
+                  <a>
+                    Build #
+                    {buildNumber}
+                  </a>
+                </div>
+              </div>
+              <Col md={12} className="state">
+                <Col sm={12} md={7}>
+                  <Col md={6}>
+                    <div className="info status">{phase}</div>
+                    <div className="info logs">
+                      <a
+                        href={jenkinsConsoleLogUrl}
+                        className="left-margin-link"
+                      >
+                        View log
                       </a>
                     </div>
-                  </div>
-                  <Col md={12} className="state">
-                    <Col sm={12} md={7}>
-                      <Col md={6}>
-                        <div className="info status">{phase}</div>
-                        <div className="info logs">
-                          <a className="left-margin-link">View log</a>
-                        </div>
-                      </Col>
-                      <Col md={6}>
-                        <div className="info creation build-timestamp">
-                          <Moment fromNow>
-                            {mobileClientBuild.status.startTimestamp}
-                          </Moment>
-                        </div>
-                      </Col>
-                    </Col>
-                    <Col sm={12} md={5}>
-                      <span>Duration: </span>
-                      <span>{durationString}</span>
-                    </Col>
+                  </Col>
+                  <Col md={6}>
+                    <div className="info creation build-timestamp">
+                      <Moment fromNow>
+                        {startTimestamp}
+                      </Moment>
+                    </div>
                   </Col>
                 </Col>
-                <Col xs={3} sm={2} md={2} className="download">
-                  <Button bsSize="xsmall">
-                                    Download
-                  </Button>
+                <Col sm={12} md={5}>
+                  <span>Duration: </span>
+                  <span>{durationString}</span>
                 </Col>
               </Col>
-            );
-          })}
-        </React.Fragment>
-      );
-    }
-}
+            </Col>
+            <Col xs={3} sm={2} md={2} className="download">
+              <Button bsSize="xsmall">
+                                Download
+              </Button>
+            </Col>
+          </Col>
+        );
+      })}
+    </React.Fragment>
+  );
+};
 
 export default MobileClientBuildHistoryList;
