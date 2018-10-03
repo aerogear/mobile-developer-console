0df45c6ab1758282a66632790ffad8ff2e66b59d
Pavel Šturc <psturc@redhat.com>
2018-10-03 16:18:52 +0200

    fix: generate and add buildUrl to each build

--- pkg/web/buildsHandler.go
+++ pkg/web/buildsHandler.go
@@ -1,10 +1,12 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/aerogear/mobile-developer-console/pkg/mobile"
 	"github.com/labstack/echo"
+	buildV1 "github.com/openshift/api/build/v1"
 )
 
 type MobileBuildsHandler struct {
@@ -13,9 +15,14 @@ type MobileBuildsHandler struct {
 	openshiftMasterURL string
 }
 
-type response struct {
-	mobile.BuildList
-	OpenshiftMasterURL string `json:"openshiftMasterUrl,inline"`
+type extendedBuildList struct {
+	buildV1.BuildList
+	Items []extendedBuild `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
+
+type extendedBuild struct {
+	buildV1.Build
+	BuildURL string `json:"buildUrl,inline"`
 }
 
 func NewMobileBuildsHandler(buildsCRUDL mobile.BuildCRUDL, namespace string, openshiftMasterURL string) *MobileBuildsHandler {
@@ -26,15 +33,34 @@ func NewMobileBuildsHandler(buildsCRUDL mobile.BuildCRUDL, namespace string, ope
 	}
 }
 
+// Extend existing struct buildV1.BuildList so each build contains URL
+// pointing to build in OpenShift Console
+func extendBuildList(bl buildV1.BuildList, mbh MobileBuildsHandler) *extendedBuildList {
+	extendedBuilds := make([]extendedBuild, len(bl.Items))
+
+	for i, build := range bl.Items {
+		buildConfigName := build.Status.Config.Name
+		buildName := build.ObjectMeta.Name
+		buildURL := fmt.Sprintf(
+			"%s/console/project/%s/browse/pipelines/%s/%s",
+			mbh.openshiftMasterURL, mbh.namespace, buildConfigName, buildName,
+		)
+		extendedBuilds[i] = extendedBuild{
+			Build:    build,
+			BuildURL: buildURL,
+		}
+	}
+	return &extendedBuildList{
+		BuildList: bl,
+		Items:     extendedBuilds,
+	}
+}
+
 func (mbh *MobileBuildsHandler) List(c echo.Context) error {
 	builds, err := mbh.buildsCRUDL.List(mbh.namespace)
 	if err != nil {
 		c.Logger().Errorf("error listing builds %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
-	response := &response{
-		BuildList:          *builds,
-		OpenshiftMasterURL: mbh.openshiftMasterURL,
-	}
-	return c.JSON(http.StatusOK, response)
+	return c.JSON(http.StatusOK, extendBuildList(*builds, *mbh))
 }
