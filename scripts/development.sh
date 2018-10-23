29145f2ba9a8e5226f75554ba9cd5760c11a3de5
Wei Li <wei.li@redhat.com>
2018-10-23 17:13:16 +0100

    enable the build tab for development

--- scripts/development.sh
+++ scripts/development.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$1" == "go-server" ]; then
-    DEBUG=true ./mobile-developer-console -kubeconfig ~/.kube/config &>server.log
+    DEBUG=true ENABLE_BUILD_TAB=true ./mobile-developer-console -kubeconfig ~/.kube/config &>server.log
 fi
 
 if [ "$1" == "kill-go-server" ]; then
