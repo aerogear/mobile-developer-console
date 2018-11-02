170b5555cc7f807db78865e2d0ec5d3b6a5dfedd
Jan Hellar <jhellar@redhat.com>
2018-11-02 13:14:57 +0100

    fix: exit if openshift setup fails

--- scripts/minishift.sh
+++ scripts/minishift.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 minishift delete -f
 
 MINISHIFT_ENABLE_EXPERIMENTAL=y minishift start --openshift-version v3.11.0 \
---extra-clusterup-flags "--enable=*,service-catalog,automation-service-broker,template-service-broker"
+--extra-clusterup-flags "--enable=*,service-catalog,automation-service-broker,template-service-broker" || exit 1
 
 oc login -u system:admin
 
