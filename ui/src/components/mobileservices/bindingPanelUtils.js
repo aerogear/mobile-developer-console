7b6f9efc643806aacbfac65c21cd2c8f481b6b69
Summers Pittman <secondsun@gmail.com>
2018-11-21 10:24:39 -0500

    Dropdowns use default values

--- ui/src/components/mobileservices/bindingPanelUtils.js
+++ ui/src/components/mobileservices/bindingPanelUtils.js
@@ -43,7 +43,7 @@ function getOpenShiftField(field, properties, idSchema, schema) {
       );
     } else if (field === 'CLIENT_TYPE') {
       const enumArray = schema.properties[field].enum;
-
+      const defaultValue = schema.properties[field].default;
       return (
         <div key={property.content.key}>
           <label className="control-label" htmlFor={id}>
@@ -52,6 +52,7 @@ function getOpenShiftField(field, properties, idSchema, schema) {
           <select
             className="form-control"
             multiple={false}
+            defaultValue={typeof defaultValue === "undefined" ? "" : defaultValue}
             id={id}
             onChange={event => {
               property.content.props.onChange(event.target.value);
@@ -66,6 +67,7 @@ function getOpenShiftField(field, properties, idSchema, schema) {
         </div>
       );
     }
+    console.log(field)
     return <div key={property.content.key}>{property.content}</div>;
   }
   return getFieldSet(field, properties, idSchema, schema);
