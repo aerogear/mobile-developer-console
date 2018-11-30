9ab8ea5ddbdd0571f1ed49e2163d41b46adacf50
Ali Ok <aliok@apache.org>
2018-11-30 14:59:59 +0300

    AEROGEAR-8137 - add some missing test cases

--- ui/src/components/mobileservices/BoundServiceRow.test.js
+++ ui/src/components/mobileservices/BoundServiceRow.test.js
@@ -1,11 +1,19 @@
 /* eslint guard-for-in: 0 */
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import BoundServiceRow from './BoundServiceRow';
 
-describe('BoundServiceRow', () => {
+const store = {
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+  getState: jest.fn()
+};
+
+describe('BoundServiceRow - not UPS', () => {
   const service = {
     getConfiguration: () => undefined,
     getConfigurationExt: () => undefined,
@@ -39,5 +47,110 @@ describe('BoundServiceRow', () => {
     ];
     const wrapper = shallow(<BoundServiceRow service={service} />);
     expect(wrapper.find(`a[href="${configurationUrl}"]`).text()).toEqual(configurationUrl);
+  });
+  it('should not render the bind button', () => {
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindButton')).toHaveLength(0);
+  });
+  it('should not render the binding status', () => {
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindingStatus')).toHaveLength(0);
+  });
+});
+
+describe('BoundServiceRow - UPS - 1 binding', () => {
+  const service = {
+    getConfiguration: () => undefined,
+    getConfigurationExt: () => `[
+      {"type":"android","typeLabel":"Android","url":"https://ups-mdc.127.0.0.1.nip.io/#/app/8936dead-7552-4b55-905c-926752c759af/variants/2d76d1eb-65ef-471c-8d21-75f80c3f370f","id":"2d76d1eb-65ef-471c-8d21-75f80c3f370f"}
+    ]`,
+    getDocumentationUrl: () => undefined,
+    getBindingName: () => 'test-ups-r66b9',
+    getIconClass: () => 'fa fa-something',
+    getId: () => 'UPS',
+    getLogoUrl: () => undefined,
+    getName: () => 'UPS',
+    isUPSService: () => true,
+    isBindingOperationInProgress: () => false,
+    isBindingOperationFailed: () => false,
+    getBindingSchema: () => undefined,
+    getFormDefinition: () => undefined,
+    isBound: () => true
+  };
+  it('should render the bind button', () => {
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindButton')).toHaveLength(1);
+  });
+  it('should render the binding status', () => {
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindingStatus')).toHaveLength(1);
+  });
+});
+
+describe('BoundServiceRow - UPS - 2 bindings', () => {
+  const service = {
+    getConfiguration: () => undefined,
+    getConfigurationExt: () => `[
+      {"type":"android","typeLabel":"Android","url":"https://ups-mdc.127.0.0.1.nip.io/#/app/8936dead-7552-4b55-905c-926752c759af/variants/2d76d1eb-65ef-471c-8d21-75f80c3f370f","id":"2d76d1eb-65ef-471c-8d21-75f80c3f370f"},
+      {"type":"ios","typeLabel":"iOS","url":"https://ups-mdc.127.0.0.1.nip.io/#/app/8936dead-7552-4b55-905c-926752c759af/variants/c8d70b96-bd52-499c-845b-756089e06d36","id":"c8d70b96-bd52-499c-845b-756089e06d36"}
+    ]`,
+    getDocumentationUrl: () => undefined,
+    getBindingName: () => 'test-ups-r66b9',
+    getIconClass: () => 'fa fa-something',
+    getId: () => 'UPS',
+    getLogoUrl: () => undefined,
+    getName: () => 'UPS',
+    isUPSService: () => true,
+    isBindingOperationInProgress: () => false,
+    isBindingOperationFailed: () => false,
+    getBindingSchema: () => undefined,
+    getFormDefinition: () => undefined,
+    isBound: () => true
+  };
+  it('should not render the bind button', () => {
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindButton')).toHaveLength(0);
+  });
+  it('should render the binding status', () => {
+    // normally we show the binding status in the UnboundServiceRow, not in the bound one.
+    // but, as there might be a binding in progress, we still need to show this status in BoundServiceRow too
+    const wrapper = mount(
+      <Provider store={store} key="provider">
+        <BrowserRouter>
+          <BoundServiceRow service={service} />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(wrapper.find('BindingStatus')).toHaveLength(1);
   });
 });
