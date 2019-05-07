7a692e5adcdcdb22494de77cb570679d30e9ad1e
Wei Li <wei.li@redhat.com>
2019-05-07 11:35:05 +0100

    remove service bindings related stuff

--- ui/src/models/index.js
+++ ui/src/models/index.js
@@ -1,3 +1,2 @@
 export { default as MobileApp } from './mobileapps/mobileapp';
 export { MobileService } from './mobileservices/mobileservice';
-export { ServiceBinding } from './mobileservices/servicebinding';
