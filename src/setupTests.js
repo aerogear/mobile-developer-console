5b4330fd56d3f32bbc6a1f99167801be37036281
Enda Phelan <ephelan@redhat.com>
2019-06-14 11:25:53 +0100

    test(setup): added more properties to OPENSHIFT_CONFIG

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,13 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 window.OPENSHIFT_CONFIG = {
-  mdcNamespace: 'test'
+  mdcNamespace: 'test',
+  masterUri: 'https://mobile-developer-console.com',
+  user: {
+    accessToken: 'vlseTWA8LXFz6X5kBH2BJO2l33GRZSPZAHA2-FHGC94',
+    name: 'mockuser',
+    email: 'mockuser@example.com'
+  }
 };
+
 configure({ adapter: new Adapter() });
