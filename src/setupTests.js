21402fcc5a7ed58fda962f971cb475de54c8dc34
Enda Phelan <ephelan@redhat.com>
2019-07-03 16:39:47 +0100

    test(services/openshift.js): add tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 window.OPENSHIFT_CONFIG = {
   mdcNamespace: 'test',
   masterUri: 'https://mobile-developer-console.com',
+  wssMasterUri: 'wss://mobile-developer-console.com',
   user: {
     accessToken: 'vlseTWA8LXFz6X5kBH2BJO2l33GRZSPZAHA2-FHGC94',
     name: 'mockuser',
