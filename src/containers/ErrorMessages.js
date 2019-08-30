0d9fe5dc71e3ba0004e9aae70f0282719acf9b71
Christie Molloy <cmolloy@redhat.com>
2019-08-30 10:04:04 -0400

    fix(alerts): functionality working

--- src/containers/ErrorMessages.js
+++ src/containers/ErrorMessages.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
 import { withRouter } from 'react-router-dom';
-import { dismiss, dismissAll } from '../actions/errors'
-import './style/index.css';
+import { dismiss, dismissAll } from '../actions/errors';
 
 export class ErrorMessages extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {alertOneVisible: true};
-    this.hideAlertOne = () => this.setState({ alertOneVisible: false });
-
-  }
   componentWillMount() {
     const { history, dismissAllErrors } = this.props;
     this.unlisten = history.listen(dismissAllErrors);
@@ -23,17 +16,12 @@ export class ErrorMessages extends Component {
 
   render() {
     const { errors, dismissError } = this.props;
-    const { alertOneVisible } = this.state;
     return (
       <div className="mdc-alert-group">
         {[...new Set(errors.map(error => error.message))].map((error, index) => (
-          <React.Fragment>
-            {alertOneVisible && (
-              <Alert key={index} variant="danger" title="Danger alert title" action={<AlertActionCloseButton onClose={this.hideAlertOne} />}>
-                {error}
-              </Alert>
-            )}
-          </React.Fragment>
+          <Alert key={index} variant="danger" title="Request Failed" action={<AlertActionCloseButton onClose={() => dismissError(error)} />}>
+            {error}
+          </Alert>
         ))}
       </div>
     );
