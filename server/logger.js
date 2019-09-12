89aba5b802e5f0184b01ef4b19401030c025b455
Jim Fitzpatrick <jfitzpat@redhat.com>
2019-09-12 14:06:32 +0100

    Feat(Logging): Setup for logging in both production and dev environments

---
+++ server/logger.js
@@ -0,0 +1,52 @@
+/*
+ ** When importing the logger the module name should be add to the logger
+ ** Example import:
+ ** const logger = require('./logger')('server.js');
+ ** 'server.js' been the file where the logger is been imported into.
+ */
+
+const { createLogger, format, transports } = require('winston');
+
+const messageFormat = format.printf(
+  ({ level, label, message, timestamp }) => `${timestamp}: ${label} - ${level}: ${message}`
+);
+
+module.exports = function(fileName) {
+  const defaultConsoleFormat = format.combine(
+    format.colorize({ all: true }),
+    format.timestamp(),
+    format.label({ label: fileName }),
+    messageFormat
+  );
+
+  const options = {
+    info: {
+      console: {
+        level: 'info',
+        format: defaultConsoleFormat
+      }
+    },
+    debug: {
+      console: {
+        level: 'debug',
+        format: defaultConsoleFormat
+      },
+      file: {
+        filename: 'dev-debug.log',
+        level: 'debug',
+        format: format.combine(format.timestamp(), format.label({ label: fileName }), messageFormat)
+      }
+    }
+  };
+
+  const logger = createLogger();
+
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console(options.debug.console));
+    logger.add(new transports.File(options.debug.file)); // creates local dev-debug.log file
+  } else {
+    logger.add(new transports.Console(options.info.console));
+  }
+
+  return logger;
+};
